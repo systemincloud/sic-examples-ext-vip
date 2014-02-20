2aec8e7bd49cd4b98202420303709497b9d7ad35
mji <marek.jagielski@gmail.com>
2014-02-20 17:30:18 +0100

    add webcam-capture dependency

--- com.systemincloud.ext.vip.examples.videoclient.java/src/main/java/com/systemincloud/ext/vip/examples/videoclient/java/VideoClient.java
+++ com.systemincloud.ext.vip.examples.videoclient.java/src/main/java/com/systemincloud/ext/vip/examples/videoclient/java/VideoClient.java
@@ -1,5 +1,25 @@
 package com.systemincloud.ext.vip.examples.videoclient.java;
 
+import javax.swing.JFrame;
+
+import com.github.sarxos.webcam.Webcam;
+
 public class VideoClient {
 
+    private Webcam webcam = Webcam.getDefault();
+    
+    private JFrame window = new JFrame("System in Cloud - Video Client");
+    
+    public VideoClient() {
+        
+    }
+    
+    private void startUI() {
+        // TODO Auto-generated method stub
+    }
+    
+    public static void main(String[] args) {
+        VideoClient vc = new VideoClient();
+        vc.startUI();
+    }
 }
