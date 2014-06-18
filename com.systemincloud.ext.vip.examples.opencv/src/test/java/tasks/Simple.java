f910fb34ec4528df0a5a74886a62be20060eb7a0
mji <marek.jagielski@gmail.com>
2014-06-18 16:18:30 +0200

    Add opencv example project

---
+++ com.systemincloud.ext.vip.examples.opencv/src/test/java/tasks/Simple.java
@@ -0,0 +1,34 @@
+package tasks;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Control;
+
+@JavaTaskInfo
+public class Simple extends JavaTask {
+
+    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+    
+    @InputPortInfo(name = "In", dataType = Control.class)
+    public InputPort in;
+
+    @Override
+    public void execute() {
+        System.out.println("Welcome to OpenCV " + Core.VERSION);
+        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+        System.out.println("OpenCV Mat: " + m);
+        Mat mr1 = m.row(1);
+        mr1.setTo(new Scalar(1));
+        Mat mc5 = m.col(5);
+        mc5.setTo(new Scalar(5));
+        System.out.println("OpenCV Mat data:\n" + m.dump());
+    }
+
+}
