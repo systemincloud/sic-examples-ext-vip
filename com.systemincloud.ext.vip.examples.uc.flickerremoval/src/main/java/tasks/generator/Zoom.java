593776d71fab30a2340305bbdc2ed6612d9812e5
mji <marek.jagielski@gmail.com>
2014-09-22 23:22:09 +0200

    flicker removal : init

---
+++ com.systemincloud.ext.vip.examples.uc.flickerremoval/src/main/java/tasks/generator/Zoom.java
@@ -0,0 +1,27 @@
+package tasks.generator;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.SicParameters;
+import com.systemincloud.ext.vip.modeler.api.javatask.data.Image;
+
+@JavaTaskInfo
+@SicParameters(names = { Zoom.XX })
+public class Zoom extends JavaTask {
+
+	protected static final String XX = "xx";
+
+	@InputPortInfo(name = "In", dataType = Image.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Image.class)
+	public OutputPort out;
+
+	@Override
+	public void execute() {
+	}
+
+}
