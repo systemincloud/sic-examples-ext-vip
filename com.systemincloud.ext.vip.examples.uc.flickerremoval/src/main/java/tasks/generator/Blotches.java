dd11cfe297d4f1a91bde6cb68e294fee66e958e4
mji <marek.jagielski@gmail.com>
2014-11-13 21:34:32 +0100

    Flicker Generator : Blotches

--- com.systemincloud.ext.vip.examples.uc.flickerremoval/src/main/java/tasks/generator/Blotches.java
+++ com.systemincloud.ext.vip.examples.uc.flickerremoval/src/main/java/tasks/generator/Blotches.java
@@ -1,23 +1,72 @@
 package tasks.generator;
 
+import java.util.Random;
+
 import com.systemincloud.modeler.tasks.javatask.api.InputPort;
 import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
 import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.SicParameters;
 import com.systemincloud.modeler.tasks.javatask.api.data.Int32;
 
 @JavaTaskInfo
+@SicParameters(names = { Blotches.COLOR, 
+		                 Blotches.SIZE,
+		                 Blotches.NUMBER })
 public class Blotches extends JavaTask {
 
+	protected static final String COLOR  = "color";
+	protected static final String SIZE   = "size";
+	protected static final String NUMBER = "number";
+	
+	private Random rand = new Random();
+	
 	@InputPortInfo(name = "In", dataType = Int32.class)
 	public InputPort in;
 	@OutputPortInfo(name = "Out", dataType = Int32.class)
 	public OutputPort out;
 
+	private int cl;
+	private int nb;
+	private int sz;
+	
+	private boolean initialized = false;
+	
 	@Override
 	public void execute() {
-		out.putData(in.getData(Int32.class));
+		Int32 inData    = in.getData(Int32.class);
+		int[] inValues  = inData.getValues();
+		int   ne        = inData.getNumberOfElements();
+		int   h         = inData.getDimensions().get(0);
+		int   w         = inData.getDimensions().get(1);
+		
+		int[] outValues = new int[ne];
+		
+		if(!initialized) init();
+		
+		for(int i = 0; i < ne; i++) outValues[i] = inValues[i];
+		
+		int nob = rand.nextInt(2 * nb);
+
+		for(int i = 0; i < nob; i++) {
+			int bs = rand.nextInt(2 * sz);
+			int x = rand.nextInt(w - bs);
+			int y = rand.nextInt(h - bs);
+			for(int j = 0; j < bs; j++)
+				for(int k = 0; k < bs; k++)
+					outValues[y*w + j*w + x + k] = cl;
+		}
+		
+		
+		out.putData(new Int32(inData.getDimensions(), outValues));
+	}
+
+	private void init() {
+		cl = Integer.parseInt(getParameter(COLOR));
+		nb = Integer.parseInt(getParameter(NUMBER));
+		sz = Integer.parseInt(getParameter(SIZE));
+		initialized = true;
 	}
 }
