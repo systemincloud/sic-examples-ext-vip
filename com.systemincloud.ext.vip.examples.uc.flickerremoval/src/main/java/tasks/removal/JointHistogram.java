e30c1a58bf8952968291c9de26a6a65300126d35
mji <marek.jagielski@gmail.com>
2014-11-13 22:12:05 +0100

    FlickerRemoval : progress

---
+++ com.systemincloud.ext.vip.examples.uc.flickerremoval/src/main/java/tasks/removal/JointHistogram.java
@@ -0,0 +1,24 @@
+package tasks.removal;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Int32;
+
+@JavaTaskInfo
+public class JointHistogram extends JavaTask {
+
+	@InputPortInfo(name = "In", dataType = Int32.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Int32.class)
+	public OutputPort out;
+
+	@Override
+	public void execute() {
+		
+	}
+
+}
