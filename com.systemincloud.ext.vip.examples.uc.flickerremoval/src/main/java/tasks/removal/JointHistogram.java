ac19ff16cf1783e8c62900b3c0eaadf91ac271ca
mji <marek.jagielski@gmail.com>
2015-01-05 01:24:47 +0100

    clean

--- com.systemincloud.ext.vip.examples.uc.flickerremoval/src/main/java/tasks/removal/JointHistogram.java
+++ com.systemincloud.ext.vip.examples.uc.flickerremoval/src/main/java/tasks/removal/JointHistogram.java
@@ -1,5 +1,7 @@
 package tasks.removal;
 
+import java.util.Arrays;
+
 import com.systemincloud.modeler.tasks.javatask.api.InputPort;
 import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
 import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
@@ -11,14 +13,33 @@ import com.systemincloud.modeler.tasks.javatask.api.data.Int32;
 @JavaTaskInfo
 public class JointHistogram extends JavaTask {
 
-	@InputPortInfo(name = "In", dataType = Int32.class)
-	public InputPort in;
+	@InputPortInfo(name = "In1", dataType = Int32.class)
+	public InputPort in1;
+	@InputPortInfo(name = "In0", dataType = Int32.class)
+	public InputPort in2;
+	
 	@OutputPortInfo(name = "Out", dataType = Int32.class)
 	public OutputPort out;
-
+	
 	@Override
 	public void execute() {
+		log().debug("Count joint historgram");
 		
+		Int32 in1Data = in1.getData(Int32.class);
+		Int32 in2Data = in2.getData(Int32.class);
+		
+		int[] in1Values = in1Data.getValues();
+		int[] in2Values = in2Data.getValues();
+		
+		int size = in1Data.getNumberOfElements();
+		
+		int[] outValues = new int[256*256];
+		
+		for(int i = 0; i < size; i++) {
+			int pos = in1Values[i] + in2Values[i]*256;
+			outValues[pos]++;
+		}
+		
+		out.putData(new Int32(Arrays.asList(256, 256), outValues));
 	}
-
 }
