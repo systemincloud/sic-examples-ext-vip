b96bb067b0149bf7a2f1efabfdf8dc924090f7a9
mji <marek.jagielski@gmail.com>
2015-01-11 01:10:47 +0100

    flickerremoval : viterbi

--- com.systemincloud.ext.vip.examples.uc.flickerremoval/src/main/java/tasks/removal/Viterbi.java
+++ com.systemincloud.ext.vip.examples.uc.flickerremoval/src/main/java/tasks/removal/Viterbi.java
@@ -9,6 +9,7 @@ import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
 import com.systemincloud.modeler.tasks.javatask.api.data.Int32;
+import com.systemincloud.modeler.tasks.javatask.api.data.Int64;
 
 @JavaTaskInfo
 public class Viterbi extends JavaTask {
@@ -18,16 +19,65 @@ public class Viterbi extends JavaTask {
 	@OutputPortInfo(name = "Out", dataType = Int32.class)
 	public OutputPort out;
 
+	@OutputPortInfo(name = "W", dataType = Int64.class)
+	public OutputPort outW;
+	
 	@Override
 	public void execute() {
 		log().debug("Viterbi");
 		Int32 inData = in.getData(Int32.class);
+		int size = inData.getDimensions().get(0);
 		int[] inValues = inData.getValues();
 		
+		long tmp[] = createTempTable(inValues, size);
+		int[] outValues = generateF(tmp, size);
 		
+		out.putData(new Int32(Arrays.asList(size), outValues));
 		
-		int[] outValues = new int[256];
-		out.putData(new Int32(Arrays.asList(256), outValues));
+		outW.putData(new Int64(Arrays.asList(size, size), tmp));
+	}
+
+	public long[] createTempTable(int[] inValues, int size) {
+		long table[] = new long[size*size];
+		
+		for(int i = 1; i < size; i++) {
+			long maxFromLineBeforeUpToJ = 0;
+			for(int j = 1; j < size - 1; j++) {
+				long OneUp   = i != 1 ? table[i*size + j - size] : 0;
+				long OneLeft = table[i*size + j - 1];
+				if(OneUp > maxFromLineBeforeUpToJ) maxFromLineBeforeUpToJ = OneUp;
+				table[i*size + j] = (maxFromLineBeforeUpToJ > OneLeft ? maxFromLineBeforeUpToJ : OneLeft) + inValues[i*size + j];
+			}
+			table[i*size + size - 1] = table[i*size + size - 2];
+		}
+		return table;
+	}
+	
+	public int[] generateF(long[] tmp, int size) {
+		int[] out = new int[size];
+		
+		out[size - 1] = size - 1;
+		for(int i = size - 2; i > 0; i--) {
+			int j = out[i + 1];
+			int idx = i*size + j;
+			long l = tmp[idx];
+			int m = 0;
+			while(m < j && tmp[idx - m - 1] == l) m++;
+			int n = 0;
+			while(n < i && tmp[idx - n*size - size] == l) n++;
+			if(m != 0 && n != 0) {
+				for(int z = 0; z <= n; z++) {
+					int tmpJ = (int) (j - m + Math.ceil(z * (double) n / (double) m));
+					out[i - n + z] = tmpJ;
+				}
+				i -= n - 1;
+			} else if(n != 0) {
+				for(int z = 0; z <= n; z++) out[i--] = j;
+			} else if(m != 0) out[i] = j - m;
+			else out[i] = j;
+		}
+		out[0] = 0;
+		return out;
 	}
 
 }
---
+++ com.systemincloud.ext.vip.examples.uc.flickerremoval/src/test/java/tasks/removal/viterbi/ViterbiTest.java
@@ -0,0 +1,177 @@
+package tasks.removal.viterbi;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.javatuples.Pair;
+import org.junit.Test;
+
+import tasks.removal.Viterbi;
+
+public class ViterbiTest {
+
+	private Pair<int[], Integer> loadSquareTableInt(String fileName) {
+		List<Integer> outValues = new LinkedList<>();
+		Integer size = null;
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(ViterbiTest.class.getResourceAsStream(fileName)));
+			String line;
+			while((line = in.readLine()) != null) {
+				String[] values = line.split("\\s+");
+	        	for(String sv : values) 
+	        		if(!"".equals(sv))
+	        			outValues.add(Integer.parseInt(sv));
+	        	if(size == null) size = outValues.size();
+			}
+        	in.close();
+        } catch(IOException e) {}
+
+		int[] out = new int[outValues.size()];
+		for(int i = 0; i < outValues.size(); i++) out[i] = outValues.get(i);
+		
+		return new Pair<int[], Integer>(out, size);
+	}
+	
+	private Pair<long[], Integer> loadSquareTableLong(String fileName) {
+		List<Long> outValues = new LinkedList<>();
+		Integer size = null;
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(ViterbiTest.class.getResourceAsStream(fileName)));
+			String line;
+			while((line = in.readLine()) != null) {
+				String[] values = line.split("\\s+");
+	        	for(String sv : values)
+	        		if(!"".equals(sv))
+	        			outValues.add(Long.parseLong(sv));
+	        	if(size == null) size = outValues.size();
+			}
+        	in.close();
+        } catch(IOException e) {}
+
+		long[] out = new long[outValues.size()];
+		for(int i = 0; i < outValues.size(); i++) out[i] = outValues.get(i);
+		
+		return new Pair<long[], Integer>(out, size);
+	}
+	
+	private boolean compareTables(long[] left, long[] right) {
+		if(left.length != right.length) return false;
+		for(int i = 0; i < left.length; i++)
+			if(left[i] != right[i]) return false;
+		return true;
+		
+	}
+	
+	protected void print(int[] values) {
+		for(int i = 0; i < values.length; i++)
+			System.out.print(values[i] + " ");
+	}
+	
+	protected void print(long[] values, Integer size) {
+		for(int i = 0; i < values.length; i++) {
+			if(i%size == 0) System.out.print("\n");
+			System.out.print(values[i] + " ");
+		}
+			
+	}
+	
+	private boolean compareTables(int[] left, int[] right) {
+		if(left.length != right.length) return false;
+		for(int i = 0; i < left.length; i++)
+			if(left[i] != right[i]) return false;
+		return true;
+	}
+	
+	@Test
+	public void testCreateTempTest1() {
+		Viterbi v = new Viterbi();
+		
+		Pair<int[],  Integer> in  = loadSquareTableInt("simpleIn");
+		Pair<long[], Integer> tbl = loadSquareTableLong("simpleTbl");
+		long[] result = v.createTempTable(in.getValue0(), in.getValue1());
+//		print(result, in.getValue1());
+		assertTrue(compareTables(result, tbl.getValue0()));
+	}
+	
+	@Test
+	public void testCreateTempTest2() {
+		Viterbi v = new Viterbi();
+		
+		Pair<int[],  Integer> in  = loadSquareTableInt("spaceIn");
+		Pair<long[], Integer> tbl = loadSquareTableLong("spaceTbl");
+		long[] result = v.createTempTable(in.getValue0(), in.getValue1());
+//		print(result, in.getValue1());
+		assertTrue(compareTables(result, tbl.getValue0()));
+	}
+	
+	@Test
+	public void testCreateTempTest3() {
+		Viterbi v = new Viterbi();
+		
+		Pair<int[],  Integer> in  = loadSquareTableInt("twoPathsIn");
+		Pair<long[], Integer> tbl = loadSquareTableLong("twoPathsTbl");
+		long[] result = v.createTempTable(in.getValue0(), in.getValue1());
+//		print(result, in.getValue1());
+		assertTrue(compareTables(result, tbl.getValue0()));
+	}
+	
+	@Test
+	public void testCreateTempTest4() {
+		Viterbi v = new Viterbi();
+		
+		Pair<int[],  Integer> in  = loadSquareTableInt("borderIn");
+		Pair<long[], Integer> tbl = loadSquareTableLong("borderTbl");
+		long[] result = v.createTempTable(in.getValue0(), in.getValue1());
+		print(result, in.getValue1());
+		assertTrue(compareTables(result, tbl.getValue0()));
+	}
+
+	@Test
+	public void testCreateOutTest1() {
+		Viterbi v = new Viterbi();
+		
+		Pair<long[], Integer> tbl = loadSquareTableLong("simpleTbl");
+		Pair<int[],  Integer> out = loadSquareTableInt ("simpleOut");
+		int[] result = v.generateF(tbl.getValue0(), tbl.getValue1());
+//		print(result);
+		assertTrue(compareTables(result, out.getValue0()));
+	}
+	
+	@Test
+	public void testCreateOutTest2() {
+		Viterbi v = new Viterbi();
+		
+		Pair<long[], Integer> tbl = loadSquareTableLong("spaceTbl");
+		Pair<int[],  Integer> out = loadSquareTableInt ("spaceOut");
+		int[] result = v.generateF(tbl.getValue0(), tbl.getValue1());
+//		print(result);
+		assertTrue(compareTables(result, out.getValue0()));
+	}
+	
+	@Test
+	public void testCreateOutTest3() {
+		Viterbi v = new Viterbi();
+		
+		Pair<long[], Integer> tbl = loadSquareTableLong("twoPathsTbl");
+		Pair<int[],  Integer> out = loadSquareTableInt ("twoPathsOut");
+		int[] result = v.generateF(tbl.getValue0(), tbl.getValue1());
+//		print(result);
+		assertTrue(compareTables(result, out.getValue0()));
+	}
+	
+	@Test
+	public void testCreateOutTest4() {
+		Viterbi v = new Viterbi();
+		
+		Pair<long[], Integer> tbl = loadSquareTableLong("borderTbl");
+		Pair<int[],  Integer> out = loadSquareTableInt ("borderOut");
+		int[] result = v.generateF(tbl.getValue0(), tbl.getValue1());
+		print(result);
+		assertTrue(compareTables(result, out.getValue0()));
+	}
+}
